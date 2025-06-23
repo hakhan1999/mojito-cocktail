db56b40390262d5ac53fe6095829632c0c89380b
hakhan1999 <khanhassaan961@gmail.com>
2025-06-23 13:37:44 +0500

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import gsap from "gsap"
+import { ScrollTrigger, SplitText } from "gsap/all"
+
+gsap.registerPlugin(ScrollTrigger, SplitText)
+
+const App = () => {
+  return (
+    <div>
+      Hassan Ahmed Khan
+    </div>
+  )
+}
+
+export default App
