da0eee2a8ef474d776320b20251dfdd80a71a56c
hakhan1999 <khanhassaan961@gmail.com>
2025-06-23 14:21:39 +0500

    Assets added and Navbar Component Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import gsap from "gsap"
-import { ScrollTrigger, SplitText } from "gsap/all"
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
 
-gsap.registerPlugin(ScrollTrigger, SplitText)
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div>
-      Hassan Ahmed Khan
-    </div>
-  )
-}
+    <main>
+      <Navbar />
+    </main>
+  );
+};
 
-export default App
+export default App;
